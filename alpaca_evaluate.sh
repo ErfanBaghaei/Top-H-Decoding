4487fd94f778bbc82283ff4c5aeb2d51d573ff27
Erfan Baghaei Potraghloo <baghaeip@kaveh.usc.edu>
2025-08-17 21:16:26 -0700

    alpaca sh

--- alpaca_evaluate.sh
+++ alpaca_evaluate.sh
@@ -1,11 +1,13 @@
+OUTPUT=./test.json
+
 python3 -u alpaca_generate.py \
-  --save_address ./test.json \
+  --save_address "$OUTPUT" \
   --model_name meta-llama/Llama-3.1-8B-Instruct \
   --max_new_tokens 1024 \
   --temperature 2.0 \
   --top_p 0.9 \
   --do_sample \
-  --item_cap 100 \
-  --alpha 0.4 \
+  --item_cap 5 \
+  --alpha 0.4
 
-
+alpaca_eval --annotators_config 'gpt4' --model_outputs "$OUTPUT"
